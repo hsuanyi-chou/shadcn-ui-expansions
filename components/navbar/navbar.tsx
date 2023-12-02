50cad9277101fd156ac283ae263df3a0ffe71533
Ryan Chou <ryan.chou@gogolook.com>
2023-12-02 21:20:47 +0800

    feat: copy shadn/ui navbar style

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import { DarkModeToggle } from '@/components/navbar/darkmode-toggle';
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between p-4">
+    <nav className="sticky top-0 z-50 flex w-full justify-between border-b bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <DarkModeToggle />
     </nav>
   );
