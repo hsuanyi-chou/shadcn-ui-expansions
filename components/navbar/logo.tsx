dd21127a6e25b726d241a8cdb839a17a01f16c8a
Ryan Chou <ryan.chou@gogolook.com>
2023-12-04 11:42:41 +0800

    feat: add logo image

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -1,12 +1,25 @@
+'use client';
 import React from 'react';
 import { Menu } from 'lucide-react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+
 const Logo = () => {
+  const { theme } = useTheme();
+
   return (
     <>
       <Menu className="cursor-pointer md:hidden" />
-      <div className="hidden md:block">
-        <span className="text-2xl font-bold">shadn/ui plugins</span>
-      </div>
+      <Link className="hidden gap-1 md:flex" href="/">
+        <Image
+          src={`/logo/logo-${theme === 'dark' ? 'dark' : 'light'}.svg`}
+          width={24}
+          height={24}
+          alt="logo"
+        />
+        <span className="text-xl font-bold">shadn/ui plugins</span>
+      </Link>
     </>
   );
 };
