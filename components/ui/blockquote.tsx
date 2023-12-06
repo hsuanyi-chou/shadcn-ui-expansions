4059d088c0e01ca63e19678ab85935ae71ff4b63
Ryan Chou <ryan.chou@gogolook.com>
2023-12-06 08:49:59 +0800

    feat: add classname

--- components/ui/blockquote.tsx
+++ components/ui/blockquote.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { cn } from '@/lib/utils';
 
-const Blockquote = ({ children }: { children: React.ReactNode }) => {
+type BlockquoteProps = {
+  children?: React.ReactNode;
+  className?: string;
+};
+
+const Blockquote = ({ children, className }: BlockquoteProps) => {
   return (
-    <div className="pr-1; relative rounded-lg border-l-8 border-l-gray-700 bg-gray-100 py-5 pl-16 font-sans text-lg italic leading-relaxed text-gray-500 before:absolute before:left-3 before:top-3 before:font-serif before:text-7xl before:text-gray-700 before:content-['\201C']">
+    <div
+      className={cn(
+        "relative rounded-lg border-l-8 border-l-gray-700 bg-gray-100 py-5 pl-16 pr-1 font-sans text-lg italic leading-relaxed text-gray-500 before:absolute before:left-3 before:top-3 before:font-serif before:text-6xl before:text-gray-700 before:content-['“']",
+        className,
+      )}
+    >
       {children}
     </div>
   );
 };
 
-const BlockquoteAuthor = ({ children }: { children: React.ReactNode }) => {
-  return <div className="pr-4 text-right font-bold text-gray-700">{children}</div>;
+const BlockquoteAuthor = ({ children, className }: BlockquoteProps) => {
+  return (
+    <span className={cn('pr-4 text-right font-bold text-gray-700', className)}>{children}</span>
+  );
 };
 
 export { Blockquote, BlockquoteAuthor };
