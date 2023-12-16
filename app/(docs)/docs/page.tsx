9e4c158ea42d9ce39cd8ec42e0292db2e94f0180
Ryan Chou <ryan.chou@gogolook.com>
2023-12-16 14:29:59 +0800

    feat: enlarge fot size

--- app/(docs)/docs/page.tsx
+++ app/(docs)/docs/page.tsx
@@ -8,10 +8,10 @@ const IntroductionPage = () => {
     <>
       <H1>Introduction</H1>
       <Blockquote className="mb-4">
-        <P className="text-gray-600">
+        <P className="text-lg text-gray-600 lg:text-xl">
           shadcn/ui is a re-usable components built using Radix UI and Tailwind CSS.
         </P>
-        <P className="text-gray-600">
+        <P className="text-lg text-gray-600 lg:text-xl">
           It is <b>NOT</b> a component library. It is a collection of re-usable components that you
           can copy and paste into your apps.
         </P>
@@ -19,14 +19,14 @@ const IntroductionPage = () => {
           Learn more about shadcn/ui.
         </Link>
       </Blockquote>
-      <P>
+      <P className="text-lg text-gray-600 lg:text-xl">
         This is built on top of shadcn/ui and expanded many useful components such as multiple
         selector, loading button, infinite scroll and more.
       </P>
-      <P className="font-bold italic">
+      <P className="text-lg font-bold italic lg:text-xl">
         The same as shadcn/ui, all components are free to use for personal and commercial.
       </P>
-      <P className="font-bold italic">
+      <P className="text-lg font-bold italic lg:text-xl">
         Just copy and paste to your project and customize to your needs. The code is yours.
       </P>
     </>
