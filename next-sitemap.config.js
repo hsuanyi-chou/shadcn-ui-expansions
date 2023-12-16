ceb375fac15cbfb4d408fc14863998778198c1bc
Ryan Chou <ryan.chou@gogolook.com>
2023-12-16 14:20:26 +0800

    chore: add netlify config and sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// import { IConfig } from 'next-sitemap';
+/** @type {import('next-sitemap').IConfig} */
+// Default code you can customize according to your requirements.
+module.exports = {
+  siteUrl: 'https://shadcnui-expansions.typeart.cc',
+  generateRobotsTxt: true,
+};
