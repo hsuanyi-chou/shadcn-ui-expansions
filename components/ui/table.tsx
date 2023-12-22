9d5334063795bb15eda225fc74d4f9c06b48281a
Ryan Chou <ryan.chou@gogolook.com>
2023-12-22 09:15:56 +0800

    feat: add border

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -5,7 +5,11 @@ import { cn } from '@/lib/utils';
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
     <div className="relative w-full overflow-auto">
-      <table ref={ref} className={cn('w-full caption-bottom text-sm', className)} {...props} />
+      <table
+        ref={ref}
+        className={cn('w-full caption-bottom border text-sm ', className)}
+        {...props}
+      />
     </div>
   ),
 );
@@ -44,7 +48,7 @@ const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTML
     <tr
       ref={ref}
       className={cn(
-        'border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
+        'border border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
         className,
       )}
       {...props}
