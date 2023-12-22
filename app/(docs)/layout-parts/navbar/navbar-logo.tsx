0c4df9038364f814e207304bb2a8aca655312779
Ryan Chou <ryan.chou@gogolook.com>
2023-12-22 10:41:24 +0800

    feat: add hamburger

--- app/(docs)/layout-parts/navbar/navbar-logo.tsx
+++ app/(docs)/layout-parts/navbar/navbar-logo.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Menu } from 'lucide-react';
 import Link from 'next/link';
 import Logo from '@/app/(docs)/layout-parts/logo/logo';
+import Hamburger from '@/app/(docs)/layout-parts/navbar/hamburger';
+import { cn } from '@/lib/utils';
+
+interface LogoLinkProps {
+  alwaysRender?: boolean;
+  className?: string;
+}
+
+export const LogoLink = ({ alwaysRender, className }: LogoLinkProps) => {
+  return (
+    <Link className={cn('hidden gap-1 md:flex', alwaysRender && 'flex', className)} href="/">
+      <Logo />
+      <span className="text-xl font-bold">shadcn/ui expansions</span>
+    </Link>
+  );
+};
 
 const NavbarLogo = () => {
   return (
     <>
-      <Menu className="cursor-pointer md:hidden" />
-      <Link className="hidden gap-1 md:flex" href="/">
-        <Logo />
-        <span className="text-xl font-bold">shadcn/ui expansions</span>
-      </Link>
+      <Hamburger />
+      <LogoLink />
     </>
   );
 };
