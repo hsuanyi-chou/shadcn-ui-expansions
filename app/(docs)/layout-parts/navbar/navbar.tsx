17d2c1d74d303d35c348e85a452bc640238b9933
Ryan Chou <ryan.chou@gogolook.com>
2023-12-22 15:40:30 +0800

    feat: add search

--- app/(docs)/layout-parts/navbar/navbar.tsx
+++ app/(docs)/layout-parts/navbar/navbar.tsx
@@ -4,14 +4,14 @@ import NavbarLogo from '@/app/(docs)/layout-parts/navbar/navbar-logo';
 import { Github } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { Input } from '@/components/ui/input';
+import { Search } from '@/app/(docs)/layout-parts/navbar/search';
 
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 flex w-full items-center justify-between border-b bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <NavbarLogo />
       <div className="flex gap-3">
-        <Input placeholder="Search components..."></Input>
+        <Search />
         <Link href="https://github.com/" target="_blank">
           <Button variant="outline" size="icon">
             <Github />
