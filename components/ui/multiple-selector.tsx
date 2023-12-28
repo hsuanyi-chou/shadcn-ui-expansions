09242ef929c764cac46d75556c5f917f36eda05c
Ryan Chou <ryan.chou@gogolook.com>
2023-12-28 10:47:17 +0800

    refactor: remove return

--- components/ui/multiple-selector.tsx
+++ components/ui/multiple-selector.tsx
@@ -224,43 +224,41 @@ export default function GroupMultipleSelector({
             ) : (
               <>
                 <CommandEmpty>{emptyIndicator}</CommandEmpty>
-                {Object.entries(selectables).map(([key, dropdowns]) => {
-                  return (
-                    <CommandGroup key={key} heading={key} className="h-full overflow-auto">
-                      <>
-                        {dropdowns.map((option) => {
-                          return (
-                            <CommandItem
-                              key={option.value}
-                              value={option.value}
-                              disabled={option.disable}
-                              onMouseDown={(e) => {
-                                e.preventDefault();
-                                e.stopPropagation();
-                              }}
-                              onSelect={(value) => {
-                                if (selected.length >= maxSelected) {
-                                  onMaxSelected?.(selected.length);
-                                  return;
-                                }
-                                setInputValue('');
-                                const newOptions = [...selected, option];
-                                setSelected(newOptions);
-                                onChange?.(newOptions);
-                              }}
-                              className={cn(
-                                'cursor-pointer',
-                                option.disable && 'cursor-default text-muted-foreground',
-                              )}
-                            >
-                              {option.label}
-                            </CommandItem>
-                          );
-                        })}
-                      </>
-                    </CommandGroup>
-                  );
-                })}
+                {Object.entries(selectables).map(([key, dropdowns]) => (
+                  <CommandGroup key={key} heading={key} className="h-full overflow-auto">
+                    <>
+                      {dropdowns.map((option) => {
+                        return (
+                          <CommandItem
+                            key={option.value}
+                            value={option.value}
+                            disabled={option.disable}
+                            onMouseDown={(e) => {
+                              e.preventDefault();
+                              e.stopPropagation();
+                            }}
+                            onSelect={(value) => {
+                              if (selected.length >= maxSelected) {
+                                onMaxSelected?.(selected.length);
+                                return;
+                              }
+                              setInputValue('');
+                              const newOptions = [...selected, option];
+                              setSelected(newOptions);
+                              onChange?.(newOptions);
+                            }}
+                            className={cn(
+                              'cursor-pointer',
+                              option.disable && 'cursor-default text-muted-foreground',
+                            )}
+                          >
+                            {option.label}
+                          </CommandItem>
+                        );
+                      })}
+                    </>
+                  </CommandGroup>
+                ))}
               </>
             )}
           </CommandList>
