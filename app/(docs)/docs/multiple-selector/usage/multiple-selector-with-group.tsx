bbd54c1c7cbba7dff95a4374a9c339981b76f782
Ryan Chou <ryan.chou@gogolook.com>
2023-12-28 14:26:13 +0800

    feat: add example

--- app/(docs)/docs/multiple-selector/usage/multiple-selector-with-group.tsx
+++ app/(docs)/docs/multiple-selector/usage/multiple-selector-with-group.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import MultipleSelector, { Option } from '@/components/ui/multiple-selector';
 
 const OPTIONS: Option[] = [
-  { label: 'nextjs', value: 'Nextjs', group: 'React' },
+  { label: 'nextjs', value: 'nextjs', group: 'React' },
   { label: 'React', value: 'react', group: 'React' },
   { label: 'Remix', value: 'remix', group: 'React' },
+  { label: 'shadcn-ui', value: 'shadcn-ui', group: 'React' },
+  { label: 'mui', value: 'mui', group: 'React' },
   { label: 'Vite', value: 'vite', group: 'Vue' },
   { label: 'Nuxt', value: 'nuxt', group: 'Vue' },
   { label: 'Vue', value: 'vue', group: 'Vue' },
+  { label: 'Quasar', value: 'quasar', group: 'Vue' },
+  { label: 'Angular', value: 'angular', group: 'Angular' },
+  { label: 'Material UI', value: 'material-ui', group: 'Angular' },
+  { label: 'Ng-zorro', value: 'ng-zorro', group: 'Angular' },
 ];
 
 const MultipleSelectorWithGroup = () => {
