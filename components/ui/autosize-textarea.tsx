1ac35a690b31302af4b601b128e1b2396d19d49f
Ryan Chou <ryan.chou@gogolook.com>
2024-01-11 09:39:27 +0800

    feat: add supports of react-hook-form

--- components/ui/autosize-textarea.tsx
+++ components/ui/autosize-textarea.tsx
@@ -59,6 +59,7 @@ export const AutosizeTextarea = React.forwardRef<AutosizeTextAreaRef, AutosizeTe
       minHeight = 52,
       className,
       onChange,
+      value,
       ...props
     }: AutosizeTextAreaProps,
     ref: React.Ref<AutosizeTextAreaRef>,
@@ -79,9 +80,16 @@ export const AutosizeTextarea = React.forwardRef<AutosizeTextAreaRef, AutosizeTe
       minHeight,
     }));
 
+    React.useEffect(() => {
+      if (value) {
+        setTriggerAutoSize(value as string);
+      }
+    }, [value]);
+
     return (
       <textarea
         {...props}
+        value={value}
         ref={textAreaRef}
         className={cn(
           'flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
