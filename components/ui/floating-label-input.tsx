08d002267e22daa03a2f641f90a61861dd209ccf
Ryan Chou <ryan.chou@gogolook.com>
2024-02-21 08:53:28 +0800

    fix: safari not floating and label bg error in dark mode

--- components/ui/floating-label-input.tsx
+++ components/ui/floating-label-input.tsx
@@ -8,7 +8,7 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
 
 const FloatingInput = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => {
-    return <Input placeholder="" className={cn('peer', className)} ref={ref} {...props} />;
+    return <Input placeholder=" " className={cn('peer', className)} ref={ref} {...props} />;
   },
 );
 FloatingInput.displayName = 'FloatingInput';
@@ -20,7 +20,7 @@ const FloatingLabel = React.forwardRef<
   return (
     <Label
       className={cn(
-        'peer-focus:secondary peer-focus:dark:secondary absolute start-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 dark:bg-gray-900 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4',
+        'peer-focus:secondary peer-focus:dark:secondary absolute start-2 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-background px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 dark:bg-background rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4',
         className,
       )}
       ref={ref}
