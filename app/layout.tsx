bdb30dd6acb14894077b73f348c5ca7fb1bb2bff
Ryan Chou <ryan.chou@gogolook.com>
2024-02-26 10:00:59 +0800

    add ad-recovery

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export const metadata: Metadata = {
       'shadcn/ui expansions collect lots of useful components which shadcn/ui does not have out of box. All the components are base on shadcn/ui. Just copy and paste. The component is yours.',
   },
   other: {
-    'google-site-verification': 'q2TjtYRJbnG5q8Am34DYea1AL_VmYHOUOLlKQGgamGQ'
-  }
+    'google-site-verification': 'q2TjtYRJbnG5q8Am34DYea1AL_VmYHOUOLlKQGgamGQ',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -47,6 +47,31 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
                   gtag('config', 'G-MNNJR7PT89');
                 `}</Script>
+      <Script
+        async
+        src="https://fundingchoicesmessages.google.com/i/pub-5359135355025668?ers=1"
+        nonce="fEfpZUxIq2eg_ICCnG1txw"
+        id="ad-funding-choices"
+      />
+      <Script nonce="fEfpZUxIq2eg_ICCnG1txw" id="ad-recover">
+        {`(function() {
+      function signalGooglefcPresent() {
+        if (!window.frames['googlefcPresent']) {
+          if (document.body) {
+            const iframe = document.createElement('iframe');
+            iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+            iframe.style.display = 'none';
+            iframe.name = 'googlefcPresent';
+            document.body.appendChild(iframe);
+          } else {
+            setTimeout(signalGooglefcPresent, 0);
+          }
+        }
+      }
+
+      signalGooglefcPresent();
+    })();`}
+      </Script>
       <body
         className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}
         suppressHydrationWarning
@@ -58,9 +83,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           disableTransitionOnChange
         >
           <Navbar />
-          <section className="container">
-            {children}
-          </section>
+          <section className="container">{children}</section>
         </ThemeProvider>
       </body>
     </html>
