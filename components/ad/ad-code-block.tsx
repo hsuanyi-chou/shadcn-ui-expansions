f1facbedb37a9ef9c8866ef4a58f2899ca758b43
Ryan Chou <ryan.chou@gogolook.com>
2024-03-02 20:00:03 +0800

    chore: add code-block ad

---
+++ components/ad/ad-code-block.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AdBase from './ad-base';
+
+const AdCodeBlock = () => {
+  return (
+    <AdBase
+      id={`shadcnui-expansions-code-block-${Math.floor(Math.random() * 1000)}`}
+      slot="9926290972"
+      className="mt-4"
+    />
+  );
+};
+
+export default AdCodeBlock;
