0087cb9789154920124a6a4007973b92baf2b06f
Ryan Chou <ryan.chou@gogolook.com>
2024-07-11 22:31:34 +0800

    feat: add hour cycle usage

--- app/(docs)/docs/datetime-picker/usage/datetime-picker-hour-cycle.tsx
+++ app/(docs)/docs/datetime-picker/usage/datetime-picker-hour-cycle.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { OldDatetimePicker } from '@/components/ui/old-datetime-picker';
+'use client';
+import React, { useState } from 'react';
+import { DateTimePicker } from '@/components/ui/datetime-picker';
+import { Label } from '@/components/ui/label';
 
 const DatetimePickerHourCycle = () => {
+  const [date12, setDate12] = useState<Date | undefined>(undefined);
+  const [date24, setDate24] = useState<Date | undefined>(undefined);
   return (
-    <div className="space-y-3">
-      <OldDatetimePicker granularity="second" hourCycle={12} />
-      <OldDatetimePicker granularity="second" hourCycle={24} />
+    <div className="flex flex-col gap-3 lg:flex-row">
+      <div className="flex flex-col gap-2">
+        <Label>12 Hour</Label>
+        <DateTimePicker hourCycle={12} value={date12} onChange={setDate12} />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Label>24 Hour</Label>
+        <DateTimePicker hourCycle={24} value={date24} onChange={setDate24} />
+      </div>
     </div>
   );
 };
