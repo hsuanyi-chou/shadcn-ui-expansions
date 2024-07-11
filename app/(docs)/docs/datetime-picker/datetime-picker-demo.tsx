165374899a9cefee99f25e8ce7f95351257f6967
Ryan Chou <ryan.chou@gogolook.com>
2024-07-11 17:54:30 +0800

    feat: add displayFormat, disabled props and some comments

--- app/(docs)/docs/datetime-picker/datetime-picker-demo.tsx
+++ app/(docs)/docs/datetime-picker/datetime-picker-demo.tsx
@@ -1,11 +1,10 @@
 'use client';
 import React, { useState } from 'react';
 import { DateTimePicker } from '@/components/ui/datetime-picker';
-import { zhTW } from 'date-fns/locale/zh-TW';
 
 const DatetimePickerDemo = () => {
   const [date, setDate] = useState<Date | undefined>(undefined);
-  return <DateTimePicker date={date} onChange={setDate} hourCycle={12} />;
+  return <DateTimePicker date={date} onChange={setDate} />;
 };
 
 export default DatetimePickerDemo;
