d433919c712a9feddbe24f4d7835d6ddf52315d1
Ryan Chou <ryan.chou@gogolook.com>
2024-09-11 10:11:43 +0800

    style: add max-h-[90%] for small screen(#141)

--- components/ui/responsive-modal.tsx
+++ components/ui/responsive-modal.tsx
@@ -38,9 +38,9 @@ const ResponsiveModalVariants = cva(
   {
     variants: {
       side: {
-        top: 'inset-x-0 top-0 border-b rounded-b-xl lg:h-fit data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',
+        top: 'inset-x-0 top-0 border-b rounded-b-xl max-h-[90%] lg:h-fit data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',
         bottom:
-          'inset-x-0 bottom-0 border-t lg:h-fit rounded-t-xl data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
+          'inset-x-0 bottom-0 border-t lg:h-fit max-h-[90%] rounded-t-xl data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
         left: 'inset-y-0 left-0 h-full lg:h-fit w-3/4 border-r rounded-r-xl data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm',
         right:
           'inset-y-0 right-0 h-full lg:h-fit w-3/4 border-l rounded-l-xl data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
